5ab616717c435205f95a40680ddc8ee79b9272c2
Bryan Miller <bmiller1008@gmail.com>
2018-08-07 13:52:04 -0400

    Just initial creation of the GameMap node.

--- game/nodes.py
+++ game/nodes.py
@@ -1,11 +1,11 @@
 
-from .gbe.nodes import *
+from . import gbe
 import pygame
 
-class NodeInterface(NodeSurface):
+class NodeInterface(gbe.nodes.NodeSurface):
     def __init__(self, name="Interface", parent=None):
         try:
-            NodeSurface.__init__(self, name, parent)
+            gbe.nodes.NodeSurface.__init__(self, name, parent)
         except NodeError as e:
             raise e
 
@@ -14,3 +14,14 @@ class NodeInterface(NodeSurface):
         self.draw_rect((0, 0, size[0], 10), pygame.Color(255,0,0,128), 1)
         self.draw_circle((int(size[0]/2), int(size[1]/2)), 16, pygame.Color(255,0,0,255), 2, pygame.Color(0,255,0,255))
 
+
+
+class NodeGameMap(gbe.nodes.Node2D):
+    def __init__(self, name="GameMap", parent=None):
+        try:
+            gbe.nodes.Node2D.__init__(self, name, parent)
+        except NodeError as e:
+            raise e
+
+    def on_render(self):
+        pass
