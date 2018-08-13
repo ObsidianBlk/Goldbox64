550ccdbb06cc23f0596fe0e36484242a9330cf53
Bryan Miller <bmiller1008@gmail.com>
2018-08-13 14:01:08 -0400

    Modules in the game/scenes directory will be for organizing complex node scenes

---
+++ game/scenes/editor.py
@@ -0,0 +1,22 @@
+
+from .. import gbe
+from ..nodes import *
+
+_TREE = None
+def get():
+    global _TREE
+    if _TREE is None:
+        root = gbe.nodes.NodeSurface("Editor")
+        root.scale_to_display = True
+        root.keep_aspect_ratio = True
+        root.align_center = True
+        root.set_surface((64, 64))
+
+        gamemap = NodeGameMap("GameMap", root)
+        gamemap.set_resources("", "walls.json")
+        gamemap.add_layer("main", 10, 10)
+
+        editor = NodeMapEditor("Editor", gamemap)
+
+        _TREE = root
+    return _TREE
